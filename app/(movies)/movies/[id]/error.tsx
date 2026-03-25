f975c5108bdf94c3007fcaf11effce65e7abe697
dayeons <dy7326@gmail.com>
2026-03-26 00:21:34 +0900

    feat: data fetching

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ErrorOMG() {
+  return <h2>something broke...</h2>;
+}
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,22 @@
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-video";
+
 export default async function MovieDetail({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  return <h1>movie {id}</h1>;
+  // const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  return (
+    <div>
+      <Suspense fallback={<h2>loading movie info</h2>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h2>loading movie video</h2>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
